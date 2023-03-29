ec98cc20e1abfed3a93c16104c58dacce7c95211
Ruben van Staden <rvs.cipher@proton.me>
2023-03-28 19:36:55 -0600

    test: add first integration test

---
+++ tests/health_check.rs
@@ -0,0 +1,18 @@
+// No .await call, therefore no need for `spawn_app` to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
+fn spawn_app() {
+    let server = rust_email::run().expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    // No .await, no .expect
+    spawn_app();
+// [...]
+}
