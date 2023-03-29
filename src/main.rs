f3a0b15a4ef38ff7ab2e0d7f815fea47cd8d85f9
Ruben van Staden <rvs.cipher@proton.me>
2023-03-29 13:40:17 -0600

    feat: basic logging with env setting

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use rust_email::config::get_configuration;
 use rust_email::startup::run;
 use std::net::TcpListener;
 use sqlx::PgPool;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above 
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io:
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Get a pointer copy and attach it to the application state
